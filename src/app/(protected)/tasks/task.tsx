cc78e825dc2f2918d6366db52983e04e2f954ae7
thayna-bezerra <arraisthayna2106@gmail.com>
2025-05-18 17:09:43 -0300

    feat: created task detail screen

--- src/app/(protected)/tasks/task.tsx
+++ src/app/(protected)/tasks/task.tsx
@@ -1,28 +1,150 @@
-import { Task } from '@/src/types/task-types';
-import { Link, router, useLocalSearchParams } from 'expo-router';
-import { Linking, Text, View } from 'react-native';
+import { Task } from "../../../types/task-types";
+import { Link, useLocalSearchParams } from "expo-router";
+import { ActivityIndicator, Linking, Platform, SafeAreaView, ScrollView, StatusBar, StyleSheet, View } from "react-native";
+import { Text } from "react-native-paper"
+import { Button } from "react-native-magnus";
+import Colors from "../../../constants/colors"
+import { useState } from "react";
+import { MaterialIcons } from "@expo/vector-icons"
+
 
 export default function TaskDetails() {
     const params = useLocalSearchParams();
+    const [loading, setLoading] = useState(false)
     const task: Task = params as unknown as Task;
 
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text
-                onPress={() => {
-                    router.back();
-                }}
-            > Voltar </Text>
+        <View style={{ flex: 1, backgroundColor: "#fff" }}>
+            <View style={styles.header}>
+                <View style={styles.textContainer}>
+                    <Text style={{ ...styles.title, fontFamily: "Poppins_700Bold" }}>
+                        {task.title}
+                    </Text>
+                    <Text style={{ ...styles.subtitle, fontFamily: "Poppins_600SemiBold" }}>
+                        {task.matter}
+                    </Text>
+                </View>
 
-            <Text>Pagina Detalhes da Tarefa</Text>
-            <Text>Nome: {task.title}</Text>
-            <Text>Descrição: {task.dateDetailsInPortuguese}</Text>
+                <View style={styles.date}>
+                    <MaterialIcons name="schedule" size={18} color="#fbbf24" />
+                    <Text style={{ ...styles.dateText, fontFamily: "Poppins_500Medium" }}>
+                        {task.dateDetailsInPortuguese}
+                    </Text>
+                </View>
+            </View>
 
-            <Text
-                onPress={() => {
-                    Linking.openURL(task.url);
-                }}
-            >Redirecionar</Text>
+            <SafeAreaView style={{ flex: 1 }}>
+                <ScrollView contentContainerStyle={styles.contentContainer}>
+                    <View style={styles.titleContent}>
+                        <Text style={{ ...styles.title, fontFamily: "Poppins_700Bold" }}>
+                            Descrição
+                        </Text>
+                    </View>
+
+                    <Text style={{ ...styles.descriptionDetails, fontFamily: "Poppins_400Regular" }}>
+                        Documento Final de Modelagem e Documentação Técnica
+                        Objetivos:
+                        Consolidar a modelagem em um único documento formal.
+                        Elaborar um relatório técnico descritivo da solução modelada.
+                        Entregáveis. Documento final contendo:
+                        Introdução e contexto do problema.
+                        Levantamento de requisitos detalhado.
+                        Todos os diagramas modelados.
+                        Descrição textual explicando a modelagem.
+                        Critérios de Avaliação:
+                        Qualidade do documento final e organização das informações.
+                        Adequação dos modelos ao problema proposto.
+                        Coerência na explicação da modelagem.
+                        Consolidar a modelagem em um único documento formal.
+                        Elaborar um relatório técnico descritivo da solução modelada.
+                        Entregáveis. Documento final contendo:
+                        Introdução e contexto do problema.
+                        Levantamento de requisitos detalhado.
+                        Todos os diagramas modelados.
+                    </Text>
+                </ScrollView>
+
+                <View style={styles.buttonWrapper}>
+                    <Button
+                        block
+                        rounded="lg"
+                        h={50}
+                        bg={Colors.light.primaryDark}
+                        disabled={loading}
+                        onPress={() => Linking.openURL(task.url)}
+                    >
+                        {loading ? (
+                            <ActivityIndicator color={Colors.light.surface} />
+                        ) : (
+                            <Text
+                                variant="titleMedium"
+                                style={{
+                                    color: Colors.light.textLight,
+                                    fontFamily: "Poppins_600SemiBold",
+                                }}
+                            >
+                                Visualizar no Portal
+                            </Text>
+                        )}
+                    </Button>
+                </View>
+            </SafeAreaView>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    header: {
+        paddingHorizontal: 16,
+        backgroundColor: "#F8F9FA",
+        borderBottomEndRadius: 20,
+        borderBottomStartRadius: 20,
+        borderColor: "#8696BB1C",
+        borderWidth: 2,
+        paddingTop: Platform.OS === "android" ? StatusBar.currentHeight ?? 0 : 20,
+        paddingBottom: 20,
+        marginBottom: 10,
+        overflow: "hidden",
+    },
+    textContainer: {
+        marginTop: 20,
+    },
+    date: {
+        flexDirection: "row",
+        alignItems: "center",
+        marginTop: 8,
+    },
+    dateText: {
+        marginLeft: 6,
+        color: "#f59e0b",
+        fontSize: 14,
+    },
+    contentContainer: {
+        paddingHorizontal: 16,
+        paddingBottom: 100,
+    },
+    titleContent: {
+        marginBottom: 12,
+    },
+    title: {
+        color: "#0D1B34",
+        fontSize: 16,
+    },
+    subtitle: {
+        color: "#94a3b8",
+        marginTop: 4,
+        textTransform: "uppercase",
+        fontSize: 12,
+    },
+    descriptionDetails: {
+        color: "#000",
+        fontSize: 14,
+        lineHeight: 20,
+    },
+    buttonWrapper: {
+        position: "absolute",
+        bottom: 20,
+        left: 16,
+        right: 16,
+    },
+});
